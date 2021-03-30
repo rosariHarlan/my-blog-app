b40bcca5e4b4fd739ad79b76d3478c951d2b8013
Rosari Harlan <rosari.harlan@gmail.com>
2021-03-30 14:46:37 +0200

    Create repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navBar">
+        <header>
+          <h1>My Life</h1>
+        </header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <img src="img/hero.png" alt="hero image" />
+      <h2>Welcome to my simple blog</h2>
+      <button>Go to Articles</button>
+    </div>
+  );
+};
+
+export default Home;
